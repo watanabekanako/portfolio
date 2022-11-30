200b7fd91592f876c733a3ab4e28ed5ad78336a9
“Kanako_Watanabe” <109854906+watanabekanako@users.noreply.github.com>
2022-11-30 18:13:08 +0900

    ファイル構成調整

---
+++ src/pages/Blog/Blog.tsx
@@ -0,0 +1,11 @@
+import { BluetoothSearchingOutlined } from "@mui/icons-material";
+
+function BlogList() {
+  return (
+    <div>
+      <h2>Blogページ</h2>
+    </div>
+  );
+}
+
+export default BlogList;
---
+++ src/pages/Blog/index.tsx
@@ -0,0 +1,11 @@
+import { BluetoothSearchingOutlined } from "@mui/icons-material";
+
+function Blog() {
+  return (
+    <div>
+      <h2>Blogページ</h2>
+    </div>
+  );
+}
+
+export default Blog;
