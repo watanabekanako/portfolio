e6ffa5e5e1018303dc3d699997d00687e383a587
“Kanako_Watanabe” <109854906+watanabekanako@users.noreply.github.com>
2023-01-11 11:38:19 +0900

    登録実装

--- src/pages/admin/postCreate.tsx
+++ src/pages/admin/postCreate.tsx
@@ -16,30 +16,29 @@ import FormControlLabel from "@mui/material/FormControlLabel";
 import FormControl from "@mui/material/FormControl";
 import FormLabel from "@mui/material/FormLabel";
 import App from "../../componets/catchImg";
-
+import { Link } from "react-router-dom";
 function PostCreate() {
-  // やりたいこと
-  // post にセットされているのは、既存のデータ。
-  // これを編集
+  // idの取得
+  const { id } = useParams();
+
   const [post, setPost] = React.useState<
     | {
-        post: {
-          id: number;
-          title: string;
-          description: string;
-          content: string;
-          createdAt: number;
-        };
+        title?: string;
+        description?: string;
+        content?: string;
+        createdAt?: number;
+        categoryId?: number;
       }
     | undefined
   >();
-  console.log("ポスト", post);
-  // 登録するボタン
-  const handleSubmit = () => {
-    axios.post(`http://localhost:3000/posts`).then((response) => {
-      setPost(response.data);
-    });
-  };
+  // const [title, setTitle] = React.useState();
+  React.useEffect(() => {
+    if (id) {
+      axios.get(`http://localhost:3000/posts/${id}`).then((response) => {
+        setPost(response.data?.post);
+      });
+    }
+  }, []);
 
   //   その取得したIDをURLのに入れる→投稿データ取得できる
   const categoryURL = "http://localhost:3000/posts/categories";
@@ -56,9 +55,18 @@ function PostCreate() {
     });
   }, []);
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    alert("チェック状態が変更されました");
+    if (event.target.value) {
+      setPost({ ...post, categoryId: Number(event.target.value) });
+    }
   };
-  console.log("post", post);
+  // 登録するボタン
+  const handleSubmit = () => {
+    axios.post(`http://localhost:3000/posts/`, { ...post }).then((response) => {
+      // 更新後の処理
+      alert("更新完了しました");
+    });
+  };
+  console.log(post);
   return (
     <DefaultLayout>
       <Box sx={{ flexGrow: 1 }}>
@@ -79,37 +87,37 @@ function PostCreate() {
             <Box textAlign="right">
               {/* <Typography component="p">{post?.post.createdAt}</Typography> */}
             </Box>
+            <Typography>タイトル</Typography>
             <TextField
               id="outlined-basic"
               variant="outlined"
               margin="dense"
               sx={{ width: 600 }}
-              // valueで現在の値を取得
-              value={post}
+              value={post?.title}
               onChange={(e: any) => {
-                setPost(e.target.value);
+                setPost({ ...post, title: e.target.value });
               }}
             />
-            {/* 
+            <Typography>内容</Typography>
             <TextField
               id="outlined-basic"
               variant="outlined"
               margin="dense"
               sx={{ width: 600 }}
-              // valueで現在の値を取得
-              value={title}
+              value={post?.content}
               onChange={(e: any) => {
-                setTitle(e.target.value);
+                setPost({ ...post, content: e.target.value });
               }}
-            /> */}
-
+            />
             <Box textAlign="center">
-              {/* <Button
-                sx={{ marginTop: 6, backgroundColor: "#fedcac" }}
-                variant="contained"
-              >
-                一覧へ戻る
-              </Button> */}
+              <Link to={`/admin`}>
+                <Button
+                  sx={{ marginTop: 6, backgroundColor: "#fedcac" }}
+                  variant="contained"
+                >
+                  一覧へ戻る
+                </Button>
+              </Link>
             </Box>
           </Grid>
           <Grid item xs={1} sx={{ marginTop: 10 }}></Grid>
@@ -123,11 +131,15 @@ function PostCreate() {
                       aria-labelledby="demo-radio-buttons-group-label"
                       defaultValue="female"
                       name="radio-buttons-group"
+                      value={post?.categoryId}
+                      onChange={handleChange}
                     >
                       <FormControlLabel
-                        value={data.name}
+                        value={data.id}
                         label={data.name}
-                        control={<Radio onChange={handleChange} />}
+                        control={
+                          <Radio checked={data.id === post?.categoryId} />
+                        }
                       />
                     </RadioGroup>
                   </>
@@ -138,37 +150,16 @@ function PostCreate() {
                 aria-labelledby="demo-radio-buttons-group-label"
                 defaultValue="female"
                 name="radio-buttons-group"
-              >
-                <FormControlLabel
-                  value="female"
-                  control={<Radio />}
-                  label="Female"
-                />
-                <FormControlLabel
-                  value="male"
-                  control={<Radio />}
-                  label="Male"
-                />
-                <FormControlLabel
-                  value="other"
-                  control={<Radio />}
-                  label="Other"
-                />
-              </RadioGroup>
+              ></RadioGroup>
             </FormControl>
             <App />
             {/* カテゴリグループ */}
-            <CategoryList />
-            {/* タググループ */}
-
-            <TagList />
           </Grid>
         </Grid>
-        <Button onClick={handleSubmit}>登録する</Button>
+        <Button onClick={handleSubmit}>更新する</Button>
       </Box>
     </DefaultLayout>
   );
 }
 
 export default PostCreate;
-// http://localhost:3000/posts
