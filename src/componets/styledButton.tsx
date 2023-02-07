2f149e563018d2a4f37ecd574efd0bc5aabeb703
“Kanako_Watanabe” <109854906+watanabekanako@users.noreply.github.com>
2023-02-07 14:37:16 +0900

    BUttonコンポーネント切り分け

---
+++ src/componets/styledButton.tsx
@@ -0,0 +1,15 @@
+import { Button, ButtonProps } from "@mui/material";
+import { yellow } from "@mui/material/colors";
+import React from "react";
+
+const StyledButton: React.FC<ButtonProps> = (props) => {
+  console.log(props);
+  return (
+    <Button
+      {...props}
+      sx={{  backgroundColor: "#fedcac", color: "#fff", ...props.sx }}
+    />
+  );
+};
+
+export default StyledButton;
