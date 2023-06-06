91deeea8cff5e699193ad0d9095d98d05182155a
“Kanako_Watanabe” <109854906+watanabekanako@users.noreply.github.com>
2023-06-06 14:57:30 +0900

    修正途中

--- src/pages/admin/postCreate.tsx
+++ src/pages/admin/postCreate.tsx
@@ -8,6 +8,7 @@ import {
   Button,
   FormHelperText,
   Snackbar,
+  Stack,
   Typography,
 } from "@mui/material";
 import { useNavigate, useParams } from "react-router-dom";
@@ -18,12 +19,25 @@ import RadioGroup from "@mui/material/RadioGroup";
 import FormControlLabel from "@mui/material/FormControlLabel";
 import FormControl from "@mui/material/FormControl";
 import FormLabel from "@mui/material/FormLabel";
-import CatchImg from "../../componets/catchImg";
 import { Link } from "react-router-dom";
 import { WithContext as ReactTags } from "react-tag-input";
 import StyledButton from "../../componets/styledButton";
 import ImageUploader from "../../componets/imgupload";
-
+import { useForm, SubmitHandler, Controller } from "react-hook-form";
+import useGetAnPost from "../../hooks/useGetAnPost";
+import useGetCategory from "../../hooks/useGetCategory";
+type Inputs = {
+  title?: string;
+  description?: string;
+  content?: string;
+  createdAt?: number;
+  categoryId?: number;
+  tags?: {
+    id?: number;
+    name: string;
+  }[];
+  thumbnailUrl?: string;
+};
 const KeyCodes = {
   comma: 188,
   enter: 13,
@@ -50,11 +64,10 @@ function isNumber(value: any): boolean {
   return !Number.isNaN(parseInt(value));
 }
 
-function PostCreate() {
-  // idの取得
-  const { id } = useParams();
+const PostCreate = () => {
   const navigate = useNavigate();
-
+  const category = useGetCategory();
+  console.log(category, "category");
   const [post, setPost] = React.useState<PostInput | undefined>();
   const [formErrors, setFormErrors] = useState<PostErrors>({});
 
@@ -63,38 +76,12 @@ function PostCreate() {
     "success" | "error" | undefined
   >("success");
 
-  React.useEffect(() => {
-    if (id) {
-      axios.get(`http://localhost:3000/posts/${id}`).then((response) => {
-        setPost(response.data?.post);
-      });
-    }
-  }, []);
-
-  //   その取得したIDをURLのに入れる→投稿データ取得できる
-  const categoryURL = "http://localhost:3000/posts/categories";
-  // カテゴリをエンドポイントからaxiosにて取得
-  const [category, setCategory] = React.useState<
-    | {
-        categories: { id: number; name: string }[];
-      }
-    | undefined
-  >();
-
   const tagURL = "http://localhost:3000/posts/tags";
   const [tags, setTags] = React.useState<
     { id: number; name: string }[] | undefined
   >();
 
   React.useEffect(() => {
-    axios
-      .get(categoryURL)
-      .then((response) => {
-        setCategory(response.data);
-      })
-      .catch((e) => {
-        setSnackMessage("データの取得に失敗しました");
-      });
     axios
       .get(tagURL)
       .then((response) => {
@@ -109,211 +96,226 @@ function PostCreate() {
       setPost({ ...post, categoryId: Number(event.target.value) });
     }
   };
+  // react-hook-form
+  const { control, handleSubmit } = useForm<Inputs>({
+    defaultValues: { title: "", description: "", content: "" },
+  });
+  const validationRules = {
+    title: {
+      required: "タイトルを入力してください",
+      minLength: { value: 4, message: "4文字以上で入力してください" },
+    },
+    description: {
+      required: "descriptionを入力してください",
+      minLength: { value: 10, message: "10文字以上で入力してください" },
+    },
+    content: {
+      required: "ブログの内容を入力してください",
+      minLength: { value: 10, message: "10文字以上で入力してください" },
+    },
+  };
 
-  // 全項目のエラーを確認してエラーオブジェクトを返す関数
-  const validate = (post: any) => {
-    const errors: PostErrors = {};
-    if (!post.title) {
-      errors.title = "タイトルを入力してください。";
-    }
-    if (!post.content) {
-      errors.content = "内容を入力してください。";
-    }
-    if (!post.categoryId) {
-      errors.categoryId = "カテゴリを選択してください。";
-    }
-    if (!post.description) {
-      errors.description = "descriptionを入力してください";
-    }
-    return errors;
+  const onSubmit: SubmitHandler<Inputs> = (data: Inputs) => {
+    axios.post(`http://localhost:3000/posts/`, { ...post }).then((response) => {
+      setSnackSeverity("success");
+      setSnackMessage("投稿が完了しました。");
+      navigate(`/blog/`);
+    });
   };
-  // 登録するボタン
-  const handleSubmit = () => {
-    const errors = validate(post);
-    console.log("error", errors);
-    setFormErrors(errors);
-    // errorsの中に何もエラーが設定されていなければリクエスト
-    if (Object.keys(errors).length === 0) {
-      axios
-        .post(`http://localhost:3000/posts/`, { ...post })
-        .then((response) => {
-          const { post } = response.data;
-          // 成功時の処理
-          setSnackSeverity("success");
-          setSnackMessage("投稿が完了しました。");
-          navigate(`/blog/${post.id}`);
-        })
-        .catch((e) => {
-          setSnackSeverity("error");
-          setSnackMessage("投稿が失敗しました。");
-        });
-    }
-  };
-  console.log("url",post?.thumbnailUrl);
   return (
     <DefaultLayout>
       <Box sx={{ flexGrow: 1, mt: 10, mb: 30 }}>
-        <Grid container spacing={2}>
-          <Grid item xs={9}>
-            <Typography>タイトル</Typography>
-            <TextField
-              // エラーメッセージ
-              // helperText={errors.title}
-              id="outlined-basic"
-              variant="outlined"
-              margin="dense"
-              sx={{ width: 1 }}
-              value={post?.title}
-              name="title"
-              onChange={(e: any) => {
-                setPost({ ...post, title: e.target.value });
-              }}
-              error={Boolean(formErrors.title)}
-              helperText={formErrors.title}
-            />
-            <Typography>description</Typography>
-            <TextField
-              id="outlined-basic"
-              variant="outlined"
-              margin="dense"
-              sx={{ width: 1 }}
-              value={post?.description}
-              onChange={(e: any) => {
-                setPost({ ...post, description: e.target.value });
-              }}
-              error={Boolean(formErrors.description)}
-              helperText={formErrors.description}
-            />
-            <Typography>内容</Typography>
-            <TextField
-              // 複数行入力できるように
-              multiline
-              rows={10}
-              maxRows={10}
-              id="outlined-basic"
-              variant="outlined"
-              margin="dense"
-              sx={{ width: 1 }}
-              value={post?.content}
-              onChange={(e: any) => {
-                setPost({ ...post, content: e.target.value });
-              }}
-              error={Boolean(formErrors.content)}
-              helperText={formErrors.content}
-            />
-            <Box
-              textAlign="center"
-              sx={{
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
-                mt: 2,
-                mb: 2,
-                gap: 2,
-              }}
-            >
-              <Link to={`/admin`}>
-                <Button sx={{ backgroundColor: "#fedcac" }} variant="contained">
-                  一覧へ戻る
-                </Button>
-              </Link>
-              <StyledButton onClick={handleSubmit} variant={"contained"}>
-                更新する
-              </StyledButton>
-      
-            </Box>
+        <Stack
+          component="form"
+          noValidate
+          onSubmit={handleSubmit(onSubmit)}
+          spacing={2}
+          sx={{ width: "80%" }}
+        >
+          <Grid container spacing={2}>
+            <Grid item xs={9}>
+              <Typography>タイトル</Typography>
 
-          </Grid>
-          <Grid item xs={3} sx={{ marginTop: 4 }}>
-            <Paper sx={{ p: 2 }}>
-              <FormLabel
-                id="demo-radio-buttons-group-label"
-                sx={{ display: "block" }}
-              >
-                カテゴリ
-              </FormLabel>
-              <FormControl>
-                {category?.categories.map((data) => {
-                  return (
-                    <>
-                      <RadioGroup
-                        aria-labelledby="demo-radio-buttons-group-label"
-                        defaultValue="female"
-                        name="radio-buttons-group"
-                        value={post?.categoryId}
-                        onChange={handleChange}
-                      >
-                        <FormControlLabel
-                          value={data.id}
-                          label={data.name}
-                          control={
-                            <Radio checked={data.id === post?.categoryId} />
-                          }
-                        />
-                      </RadioGroup>
-                    </>
-                  );
-                })}
-                <FormHelperText error={Boolean(formErrors.categoryId)}>
-                  {formErrors.categoryId}
-                </FormHelperText>
-              </FormControl>
-              <Typography>タグ</Typography>
-              <ReactTags
-                tags={post?.tags?.map((tag) => ({
-                  id: String(tag.id),
-                  text: tag.name,
-                }))}
-                suggestions={tags?.map((tag) => ({
-                  id: String(tag.id),
-                  text: tag.name,
-                }))}
-                delimiters={[KeyCodes.comma, KeyCodes.enter]}
-                handleDelete={(index) => {
-                  setPost({
-                    ...post,
-                    tags: post?.tags?.filter((tag, i) => i !== index),
-                  });
-                }}
-                handleAddition={(value) => {
-                  setPost({
-                    ...post,
-                    tags: [
-                      ...(post?.tags ?? []),
-                      {
-                        id: isNumber(value.id) ? Number(value.id) : undefined,
-                        name: value.text,
-                      },
-                    ],
-                  });
-                }}
-                handleDrag={(tag, currPos, newPos) => {
-                  const tags = [...(post?.tags ?? [])];
-                  tags.splice(currPos, 1);
-                  tags.splice(newPos, 0, {
-                    id: Number(tag.id),
-                    name: tag.text,
-                  });
-                  setPost({
-                    ...post,
-                    tags,
-                  });
-                }}
-                inputFieldPosition="bottom"
-                autocomplete
+              <Controller
+                name="title"
+                control={control}
+                rules={validationRules.title}
+                render={({ field, fieldState }) => (
+                  <TextField
+                    // エラーメッセージ
+                    {...field}
+                    id="outlined-basic"
+                    variant="outlined"
+                    margin="dense"
+                    type="text"
+                    sx={{ width: 1 }}
+                    error={fieldState.invalid}
+                    helperText={fieldState.error?.message}
+                    value={post?.title}
+                    onChange={(e: any) => {
+                      setPost({ ...post, title: e.target.value });
+                    }}
+                  />
+                )}
               />
-           
-              <ImageUploader onUploadCompleted={(url:any) => {
-           
-                 // urlをpostのthumbnailUrlにセットする
-                setPost({
-                  ...post,
-                  thumbnailUrl:url
-                })  
-  }}/>
-            </Paper>
+              <Typography>description</Typography>
+              <Controller
+                name="description"
+                control={control}
+                rules={validationRules.description}
+                render={({ field, fieldState }) => (
+                  <TextField
+                    id="outlined-basic"
+                    variant="outlined"
+                    margin="dense"
+                    sx={{ width: 1 }}
+                    value={post?.description}
+                    onChange={(e: any) => {
+                      setPost({ ...post, description: e.target.value });
+                    }}
+                    error={fieldState.invalid}
+                    helperText={fieldState.error?.message}
+                  />
+                )}
+              />
+              <Typography>内容</Typography>
+              <Controller
+                name="content"
+                control={control}
+                rules={validationRules.content}
+                render={({ field, fieldState }) => (
+                  <TextField
+                    // 複数行入力できるように
+                    multiline
+                    rows={10}
+                    maxRows={10}
+                    id="outlined-basic"
+                    variant="outlined"
+                    margin="dense"
+                    sx={{ width: 1 }}
+                    value={post?.content}
+                    onChange={(e: any) => {
+                      setPost({ ...post, content: e.target.value });
+                    }}
+                    error={fieldState.invalid}
+                    helperText={fieldState.error?.message}
+                  />
+                )}
+              />
+              <Box
+                textAlign="center"
+                sx={{
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  mt: 2,
+                  mb: 2,
+                  gap: 2,
+                }}
+              >
+                <Link to={`/admin`}>
+                  <Button
+                    sx={{ backgroundColor: "#fedcac" }}
+                    variant="contained"
+                  >
+                    一覧へ戻る
+                  </Button>
+                </Link>
+                <StyledButton type="submit">登録する</StyledButton>
+              </Box>
+            </Grid>
+            <Grid item xs={3} sx={{ marginTop: 4 }}>
+              <Paper sx={{ p: 2 }}>
+                <FormLabel
+                  id="demo-radio-buttons-group-label"
+                  sx={{ display: "block" }}
+                >
+                  カテゴリ
+                </FormLabel>
+                <FormControl>
+                  {category?.categories.map((data: any) => {
+                    return (
+                      <>
+                        <RadioGroup
+                          aria-labelledby="demo-radio-buttons-group-label"
+                          defaultValue="female"
+                          name="radio-buttons-group"
+                          value={post?.categoryId}
+                          onChange={handleChange}
+                        >
+                          <FormControlLabel
+                            value={data.id}
+                            label={data.name}
+                            control={
+                              <Radio checked={data.id === post?.categoryId} />
+                            }
+                          />
+                        </RadioGroup>
+                      </>
+                    );
+                  })}
+                  <FormHelperText error={Boolean(formErrors.categoryId)}>
+                    {formErrors.categoryId}
+                  </FormHelperText>
+                </FormControl>
+                <Typography>タグ</Typography>
+                <ReactTags
+                  tags={post?.tags?.map((tag) => ({
+                    id: String(tag.id),
+                    text: tag.name,
+                  }))}
+                  suggestions={tags?.map((tag) => ({
+                    id: String(tag.id),
+                    text: tag.name,
+                  }))}
+                  delimiters={[KeyCodes.comma, KeyCodes.enter]}
+                  handleDelete={(index) => {
+                    setPost({
+                      ...post,
+                      tags: post?.tags?.filter((tag, i) => i !== index),
+                    });
+                  }}
+                  handleAddition={(value) => {
+                    setPost({
+                      ...post,
+                      tags: [
+                        ...(post?.tags ?? []),
+                        {
+                          id: isNumber(value.id) ? Number(value.id) : undefined,
+                          name: value.text,
+                        },
+                      ],
+                    });
+                  }}
+                  handleDrag={(tag, currPos, newPos) => {
+                    const tags = [...(post?.tags ?? [])];
+                    tags.splice(currPos, 1);
+                    tags.splice(newPos, 0, {
+                      id: Number(tag.id),
+                      name: tag.text,
+                    });
+                    setPost({
+                      ...post,
+                      tags,
+                    });
+                  }}
+                  inputFieldPosition="bottom"
+                  autocomplete
+                />
+
+                <ImageUploader
+                  onUploadCompleted={(url: any) => {
+                    // urlをpostのthumbnailUrlにセットする
+                    setPost({
+                      ...post,
+                      thumbnailUrl: url,
+                    });
+                  }}
+                />
+              </Paper>
+            </Grid>
           </Grid>
-        </Grid>
+        </Stack>
       </Box>
       <Snackbar
         anchorOrigin={{
@@ -330,6 +332,6 @@ function PostCreate() {
       </Snackbar>
     </DefaultLayout>
   );
-}
+};
 
 export default PostCreate;
